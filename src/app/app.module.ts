fa6576c9d10d997a43ca50b02dc9d1e5ccc0ea2d
John Breau <john.breau@vml.com>
2020-09-18 11:50:50 -0500

    home page with cards built

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
